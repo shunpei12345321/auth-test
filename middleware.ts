f63c671f92d02e641c6b7927be3afe76138ea4d3
shunpei12345321 <u-shunpei-2404@occ.co.jp>
2024-08-08 17:50:37 +0900

    最終変更

---
+++ middleware.ts
@@ -0,0 +1,19 @@
+// reference: https://next-auth.js.org/configuration/nextjs#middleware
+export { default } from "next-auth/middleware";
+
+/*
+// 特定のページにのみ認証を要求する場合。
+export const config = {
+  matcher: "/user/:path*",
+};
+*/
+
+// 8.8一旦コメントアウト
+// import { Logger } from '@/app/_utils/logger';
+// import type { NextRequest } from 'next/server'
+
+// // This function can be marked `async` if using `await` inside
+// export function middleware(request: NextRequest) {
+//   Logger.info("logging point, " + request.nextUrl.pathname);
+//   Logger.info(request.nextUrl.toString());
+// }
