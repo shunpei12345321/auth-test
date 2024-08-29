706653af8351a2534eba67e88a3663c6ee19c6b2
shunpei12345321 <u-shunpei-2404@occ.co.jp>
2024-08-29 13:56:32 +0900

    変更点

--- app/user/_components/user-list.tsx
+++ app/user/_components/user-list.tsx
@@ -78,6 +78,7 @@ export default function UserList(props: Props) {
 											</Button>
 										</Link>
 									</TableCell>
+									<div></div>
 									<TableCell>
 										<Button
 											onClick={() => onDelete(user.id)}
