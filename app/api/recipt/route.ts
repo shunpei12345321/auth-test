0c4bb19f3fb561f0472fa4ccb35066ca9bd24674
shunpei12345321 <u-shunpei-2404@occ.co.jp>
2024-08-30 18:04:34 +0900

    aaaa

--- app/api/recipt/route.ts
+++ app/api/recipt/route.ts
@@ -1,9 +1,9 @@
-import { ThanksCardRepository } from "@/app/_repositories/ThanksCard";
+import { prisma } from "@/app/_utils/prismaSingleton";
 import { NextResponse } from "next/server";
 
 export const dynamic = "force-dynamaic";
 
 export async function GET() {
-	const thanksCard = await ThanksCardRepository.findMany();
-	return NextResponse.json(thanksCard);
+	const roles = await prisma.user.findMany();
+	return NextResponse.json(roles, { status: 200 });
 }
